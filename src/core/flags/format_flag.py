1d47ff255632f9e8e129faed62f11df0cd2ca6dc
Illia Kupratyi <illia.kupratyi@gmail.com>
2025-09-23 20:31:24 +0200

    FormatFlag

---
+++ src/core/flags/format_flag.py
@@ -0,0 +1,21 @@
+from typing import List
+
+from src.core.exception import FlagValidatorError
+from src.core.flags.base import BaseFlag
+
+
+class FormatFlag(BaseFlag):
+    name = "format"
+    shortname = "f"
+
+    def __init__(self, value: str = "best"):
+        super().__init__(value, True)
+
+    def _validate(self) -> None:
+        if not isinstance(self.value, str):
+            raise FlagValidatorError("Format value must be a string")
+
+    def to_args(self) -> List[str]:
+        return ["--" + self.name, self.value]
+
+
--- src/main.py
+++ src/main.py
@@ -1,37 +1,20 @@
-from typing import List
-
-from src.core.flags.base import BaseFlag
-from src.core.flags.format_list_flag import FormatListFlag
+from src.core.config import DATA_DIR
+from src.core.flags.format_flag import FormatFlag
+from src.core.flags.output_paths_flag import OutputPathsFlag
 from src.core.runner import YTDLPRunner
-from src.utils.format_lister import FormatLister
-
-
-class TestFormatFlag(BaseFlag):
-    name = "format"
-
-    def __init__(self, value: str = "best"):
-        super().__init__(value, True)
-
-    def _validate(self):
-        pass
-
-    def to_args(self) -> List[str]:
-        return ["--" + self.name, self.value]
 
 def main():
     runner = YTDLPRunner()
-    # runner.add_flag(TestFormatFlag("best"))
-    # runner.add_flag(OutputPathsFlag(DATA_DIR))
-    format_lister = FormatLister()
-    print(format_lister.get_formats("https://www.youtube.com/watch?v=dQw4w9WgXcQ"))
+    runner.add_flag(FormatFlag("best"))
+    runner.add_flag(OutputPathsFlag(DATA_DIR))
 
-    #def print_line(line):
-     #   print(f"{line}")
+    def print_line(line):
+        print(f"{line}")
 
-    #result = runner.run(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-     #          on_output=print_line)
+    result = runner.run(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+               on_output=print_line)
 
-    #print(f"Finished with code {result['return_code']}")
+    print(f"Finished with code {result['return_code']}")
 
 if __name__ == '__main__':
     main()
