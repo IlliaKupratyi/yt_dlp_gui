da66f3898b1fa5abf87fa7e8e20d5c8692c11093
Illia Kupratyi <illia.kupratyi@gmail.com>
2025-10-04 00:15:07 +0200

    Final refactor

--- main.py
+++ main.py
@@ -1,48 +1,8 @@
-
 from src.app import App
-from src.controller.app_controller import AppController
-from src.core.config.config import DATA_DIR
-
-from src.core.flags.convert_thumbnails_flag import ConvertThumbnailsFlag
-from src.core.flags.embed_thumbnail_flag import EmbedThumbnailFlag
-from src.core.flags.ffmpeg_location_flag import FfmpegLocationFlag
-from src.core.flags.ignore_errors_flag import IgnoreErrorsFlag
-from src.core.flags.no_overwrites_flag import NoOverwritesFlag
-from src.core.flags.output_paths_flag import OutputPathsFlag
-from src.core.flags.preset_alias_flag import PresetAliasFlag
-from src.core.flags.sub_langs_flag import SubLangsFlag
-from src.core.flags.write_link_flag import WriteLinkFlag
-from src.core.flags.write_subs_flag import WriteSubsFlag
-from src.core.flags.write_thumbnail_flag import WriteThumbnailFlag
 from src.core.logger.logger import setup_logger
-from src.core.utils.ffmpeg_find import find_ffmpeg_path
 
-IS_DEV = True  # ← поменяйте на False в релизе
+IS_DEV = True
 logger = setup_logger(log_to_console=IS_DEV)
-
-def run_manual_tests():
-    app_controller = AppController()
-    app_controller.setup_video_properties("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-
-    app_controller.add_flag(OutputPathsFlag(DATA_DIR))
-    app_controller.add_flag(PresetAliasFlag("mp4"))
-    app_controller.add_flag(WriteLinkFlag())
-    app_controller.add_flag(WriteSubsFlag())
-    app_controller.add_flag(SubLangsFlag(["en, ja"]))
-    app_controller.add_flag(NoOverwritesFlag())
-    app_controller.add_flag(IgnoreErrorsFlag())
-    app_controller.add_flag(ConvertThumbnailsFlag("jpg"))
-    app_controller.add_flag(WriteThumbnailFlag())
-    app_controller.add_flag(FfmpegLocationFlag(find_ffmpeg_path()))
-    app_controller.add_flag(EmbedThumbnailFlag())
-
-
-    def print_line(line):
-        print(f"{line}")
-
-    thread = app_controller.start_downloading(on_output=print_line)
-    if thread:
-        thread.join()
 
 def main():
     logger.info("Application started")
